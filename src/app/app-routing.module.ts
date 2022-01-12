a21cd613d4e3c5bbcaf9b84cc77e707b74521dea
satyabratamoharana104 <satyabrata.moharana@antronsys.com>
2022-01-12 12:45:34 +0530

    routing demo files added

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { DepartmentComponent } from './components/department/department.component';
+import { EmployeeComponent } from './components/employee/employee.component';
+
+const routes: Routes = [
+
+  { path: 'emp', component: EmployeeComponent },
+  { path: 'department', component: DepartmentComponent },
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
